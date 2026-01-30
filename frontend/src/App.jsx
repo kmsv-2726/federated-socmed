187bed48a83d9aa25049406e3ae93384013e9d21
Sreepathy <sreepathy200567@gmail.com>
2026-01-30 18:18:49 +0530

    Backend File structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import Home from './pages/Home';
 import LandingPage from './pages/LandingPage';
-// import './styles/app.css';
+import './styles/app.css';
 
 function App() {
   return <LandingPage/>;
