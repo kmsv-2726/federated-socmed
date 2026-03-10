4cfb9e728e2913a9bd813b45a05696bfae591a3d
ragul c.v <ragul.spiygen@gmail.com>
2026-03-10 13:37:46 +0530

    Update authRoute.js

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,12 +1,15 @@
-import express from "express"
-import { loginUser, registerUser, unlockAccount } from "../controllers/AuthController.js";
+import express from "express";
+import { loginUser, registerUser, logoutUser, unlockAccount } from "../controllers/AuthController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
 
-const router = express.Router()
+const router = express.Router();
 
 
 router.post("/login", loginUser);
 router.post("/register", registerUser);
+router.post("/logout", verifyToken, logoutUser);
+router.post("/unlock", unlockAccount);
 router.get("/unlock", unlockAccount);
 
 
-export default router
+export default router;
